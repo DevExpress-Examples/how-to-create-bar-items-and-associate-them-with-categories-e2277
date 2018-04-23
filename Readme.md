f6a7b8afbe9094a3710d0341ac4f94bf64b858ab
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create bar items and associate them with categories
+
+
+<p>This example shows how to create a bar item and associate it with a 'Format' category.</p><p>In the example, a bar is docked to the top BarContainerControl which is created automatically.</p>
+
+<br/>
+
+
---
+++ VB/CreateBarsCategories_Ex/MainPage.xaml.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+
+Namespace CreateBarsCategories_Ex
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub checkBtnBold_CheckedChanged(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+			'Implement your event handler
+		End Sub
+	End Class
+End Namespace
